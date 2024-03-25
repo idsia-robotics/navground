d18407dbb44cef70130a1c8edca88849cb8a8ca9
Jérôme Guzzi <jerome@idsia.ch>
2024-03-25 08:47:05 +0100

    Corrected float type

--- navground_examples/src/behavior/my_behavior.h
+++ navground_examples/src/behavior/my_behavior.h
@@ -23,11 +23,11 @@ class MY_BEHAVIOR_EXPORT IdleBehavior : public Behavior {
   std::string get_type() const override { return type; }
 
  protected:
-  Vector2 desired_velocity_towards_point([[maybe_unused]] const Vector2 & point, [[maybe_unused]] float speed, [[maybe_unused]] float time_step) override {
+  Vector2 desired_velocity_towards_point([[maybe_unused]] const Vector2 & point, [[maybe_unused]] ng_float_t speed, [[maybe_unused]] ng_float_t time_step) override {
     return Vector2::Zero();
   }
 
-  Vector2 desired_velocity_towards_velocity(const Vector2 & velocity, [[maybe_unused]] float time_step) override {
+  Vector2 desired_velocity_towards_velocity(const Vector2 & velocity, [[maybe_unused]] ng_float_t time_step) override {
     return Vector2::Zero();
   }
 

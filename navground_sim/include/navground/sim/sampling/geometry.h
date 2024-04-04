947f8dfb1705d22d08b810b3d8baddc88c89b82a
Jérôme Guzzi <jerome@idsia.ch>
2024-04-04 18:08:35 +0200

    Added docs

--- navground_sim/include/navground/sim/sampling/geometry.h
+++ navground_sim/include/navground/sim/sampling/geometry.h
@@ -16,12 +16,35 @@ namespace navground::sim {
 
 using BoundingBox = geos::geom::Envelope;
 
+/**
+ * @brief      Sample discs
+ *
+ * Centers are sampled uniformly inside a bounding box and rejected if too near
+ * to any other disc, taking into account the lattice. The maximal number of
+ * rejection is bound by ``max_tries``. The function exit when too many
+ * rejection are reached, even if not enough discs have been sampled.
+ *
+ * @param      rng           The random number generator
+ * @param[in]  number        The number of discs
+ * @param[in]  bounding_box  The bounding box inside of which 
+ *                           to sample disc centers
+ * @param[in]  min_radius    The minimum value for disc radius
+ * @param[in]  max_radius    The maximum value for disc radius
+ * @param[in]  margin        The minimal distance between newly sampled discs
+ * @param[in]  disc_margin   The minimal distance between newly 
+ *                           sampled discs and discs in ``discs``
+ * @param[in]  discs         A list of discs already present.
+ * @param[in]  max_tries     The maximum sampling failures 
+ *                           before exiting the function.
+ * @param[in]  lattice_grid  The lattice grid
+ *
+ * @return     A list of valid disc
+ */
 std::vector<core::Disc> NAVGROUND_SIM_EXPORT sample_discs(
     RandomGenerator &rng, unsigned number, const BoundingBox &bounding_box,
     ng_float_t min_radius, ng_float_t max_radius, ng_float_t margin = 0.0,
-    ng_float_t disc_margin = 0.0,
-    const std::vector<core::Disc> &discs = {}, unsigned max_tries = 1000,
-    std::vector<core::Vector2> lattice_grid = {});
+    ng_float_t disc_margin = 0.0, const std::vector<core::Disc> &discs = {},
+    unsigned max_tries = 1000, std::vector<core::Vector2> lattice_grid = {});
 
 }  // namespace navground::sim
 

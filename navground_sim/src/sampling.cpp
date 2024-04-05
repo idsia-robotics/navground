ef21d6aab09fa9007af0f9e1e4ed208c8c889fbd
Jérôme Guzzi <jerome@idsia.ch>
2024-04-05 17:29:38 +0200

    Added explicit casting
    Else MSVC complains

--- navground_sim/src/sampling.cpp
+++ navground_sim/src/sampling.cpp
@@ -6,24 +6,23 @@
 
 namespace navground::sim {
 
-std::vector<core::Disc> sample_discs(RandomGenerator &rng, unsigned number,
-                                     const BoundingBox &bounding_box,
-                                     ng_float_t min_radius,
-                                     ng_float_t max_radius, ng_float_t margin,
-                                     ng_float_t disc_margin,
-                                     const std::vector<core::Disc> &discs,
-                                     unsigned max_tries,
-                                     std::vector<core::Vector2> lattice_grid) {
+std::vector<core::Disc> sample_discs(
+    RandomGenerator &rng, unsigned number, const BoundingBox &bounding_box,
+    ng_float_t min_radius, ng_float_t max_radius, ng_float_t margin,
+    ng_float_t disc_margin, const std::vector<core::Disc> &discs,
+    unsigned max_tries, std::vector<core::Vector2> lattice_grid) {
   if (lattice_grid.size() == 0) {
     lattice_grid = {core::Vector2()};
   }
   std::vector<core::Disc> rs;
   unsigned tries = 0;
 
-  std::uniform_real_distribution<ng_float_t> x{bounding_box.getMinX(),
-                                               bounding_box.getMaxX()};
-  std::uniform_real_distribution<ng_float_t> y{bounding_box.getMinY(),
-                                               bounding_box.getMaxY()};
+  std::uniform_real_distribution<ng_float_t> x{
+      static_cast<ng_float_t>(bounding_box.getMinX()),
+      static_cast<ng_float_t>(bounding_box.getMaxX())};
+  std::uniform_real_distribution<ng_float_t> y{
+      static_cast<ng_float_t>(bounding_box.getMinY()),
+      static_cast<ng_float_t>(bounding_box.getMaxY())};
   std::uniform_real_distribution<ng_float_t> r{min_radius, max_radius};
 
   while (rs.size() < number && tries <= max_tries) {

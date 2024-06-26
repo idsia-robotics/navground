f6527449ed5d585ee46ce4204454ade80b103e7f
Jérôme Guzzi <jerome@idsia.ch>
2024-06-26 09:59:19 +0200

    Refactored out py_collection

--- navground_py/include/navground_py/behavior_modulation.h
+++ navground_py/include/navground_py/behavior_modulation.h
@@ -5,6 +5,7 @@
 
 #include <memory>
 
+#include "collection.h"
 #include "navground/core/behavior.h"
 #include "navground/core/behavior_modulation.h"
 
@@ -13,19 +14,8 @@ using navground::core::BehaviorModulation;
 
 namespace py = pybind11;
 
-inline bool has_modulations_py(const py::object &obj) {
-  return py::hasattr(obj, "__py_modulations");
-}
-
-inline py::list get_modulations_py(const py::object &obj) {
-  if (!py::hasattr(obj, "__py_modulations")) {
-    obj.attr("__py_modulations") = py::list();
-  }
-  return static_cast<py::list>(py::getattr(obj, "__py_modulations"));
-}
-
 inline void add_modulation_py(const py::object &obj, const py::object value) {
-  get_modulations_py(obj).append(value);
+  add_py_item(obj, value, "modulations");
   obj.cast<Behavior &>().add_modulation(
       value.cast<std::shared_ptr<BehaviorModulation>>());
 }
@@ -34,14 +24,12 @@ inline void remove_modulation_py(const py::object &obj,
                                  const py::object value) {
   obj.cast<Behavior &>().remove_modulation(
       value.cast<std::shared_ptr<BehaviorModulation>>());
-  get_modulations_py(obj).attr("remove")(value);
+  remove_py_item(obj, value, "modulations");
 }
 
 inline void clear_modulations_py(const py::object &obj) {
   obj.cast<Behavior &>().clear_modulations();
-  if (has_modulations_py(obj)) {
-    get_modulations_py(obj).attr("clear")();
-  }
+  clear_collection_py(obj, "modulations");
 }
 
-#endif  // NAVGROUND_CORE_PY_YAML_H
+#endif  // NAVGROUND_CORE_PY_BEHAVIOR_MODULATION_H
---
+++ navground_py/include/navground_py/collection.h
@@ -0,0 +1,55 @@
+#ifndef NAVGROUND_CORE_PY_COLLECTION_H
+#define NAVGROUND_CORE_PY_COLLECTION_H
+
+/**
+ * When we create a Python object from C++,
+ * for instance evaluating a function like in:
+ *
+ * 		auto py_object = py_cls.attr("__call__")();
+ * 		auto cpp_object = py_object.cast<std::shared_ptr<CppClass>>();
+ *
+ * The python object get de-allocated exiting the scope, unless we retain a
+ * reference to it. To do that, we can use the functions
+ * in this files, which attaches the python object to another one, extending
+ * therefore its lifetime, in a private python list attribute `__py_<name>`.
+ */
+
+#include <pybind11/pybind11.h>
+
+#include <memory>
+#include <string>
+
+namespace py = pybind11;
+
+inline bool has_py_collection(const py::object &obj, const std::string &name) {
+  py::object key = py::str("__py_" + name);
+  return py::hasattr(obj, key);
+}
+
+inline py::list get_py_collection(const py::object &obj,
+                                  const std::string &name) {
+  py::object key = py::str("__py_" + name);
+  if (!py::hasattr(obj, key)) {
+    obj.attr(key) = py::list();
+  }
+  return static_cast<py::list>(py::getattr(obj, key));
+}
+
+inline void add_py_item(const py::object &obj, const py::object value,
+                        const std::string &name) {
+  get_py_collection(obj, name).append(value);
+}
+
+inline void remove_py_item(const py::object &obj, const py::object value,
+                           const std::string &name) {
+  get_py_collection(obj, name).attr("remove")(value);
+}
+
+inline void clear_collection_py(const py::object &obj,
+                                const std::string &name) {
+  if (has_py_collection(obj, name)) {
+    get_py_collection(obj, name).attr("clear")();
+  }
+}
+
+#endif  // NAVGROUND_CORE_PY_COLLECTION_H

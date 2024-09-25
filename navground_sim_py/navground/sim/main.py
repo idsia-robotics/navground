70f1251ac433852ccc63329a710ff4f965668f6a
Jérôme Guzzi <jerome@idsia.ch>
2024-09-25 20:47:45 +0200

    Exposed parser

--- navground_sim_py/navground/sim/main.py
+++ navground_sim_py/navground/sim/main.py
@@ -8,9 +8,7 @@ def config_parser(parsers, name, module):
     module.init_parser(parser)
     parser.set_defaults(func=module._main)
 
-
-def main():
-    parser = argparse.ArgumentParser(prog='navground')
+def init_parser(parser: argparse.ArgumentParser) -> None:
     parsers = parser.add_subparsers(required=True, dest='cmd', help="Subcommands")
     config_parser(parsers, 'info', info)
     config_parser(parsers, 'run', run)
@@ -18,5 +16,12 @@ def main():
     config_parser(parsers, 'sample', sample)
     config_parser(parsers, 'record_video', record_video)
     config_parser(parsers, 'replay', replay)
-    args = parser.parse_args()
+
+def parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog='navground_py')
+    init_parser(parser)
+    return parser
+
+def main():
+    args = parser().parse_args()
     args.func(args)

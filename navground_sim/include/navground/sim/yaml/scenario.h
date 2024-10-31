3ba65af9e00d57219599a4f4be67a6728df73007
Jérôme Guzzi <jerome@idsia.ch>
2024-10-31 17:18:48 +0100

    Corrected YAML encoding of walls
    Now they are encoded as walls, while before they were encoded as lines.

--- navground_sim/include/navground/sim/yaml/scenario.h
+++ navground_sim/include/navground/sim/yaml/scenario.h
@@ -18,37 +18,40 @@ using navground::sim::World;
 
 namespace YAML {
 
-template <typename W = World>
-struct convert_scenario {
+template <typename W = World> struct convert_scenario {
   using AS = AgentSampler<W>;
 
-  static Node encode(const Scenario& rhs) {
+  static Node encode(const Scenario &rhs) {
     Node node;
     encode_type_and_properties<Scenario>(node, rhs);
-    for (const auto& [name, sampler] : rhs.property_samplers) {
+    for (const auto &[name, sampler] : rhs.property_samplers) {
       if (sampler) {
         node[name] = sampler;
       }
     }
     node["obstacles"] = rhs.obstacles;
-    node["walls"] = rhs.walls;
-    for (const auto& group : rhs.groups) {
+    for (const auto &line : rhs.walls) {
+      Node wn;
+      wn["line"] = line;
+      node["walls"].push_back(wn);
+    }
+    for (const auto &group : rhs.groups) {
 #ifndef _UNSAFE_GROUP_CASTING
-      if (AS* g = dynamic_cast<AS*>(group.get()))
+      if (AS *g = dynamic_cast<AS *>(group.get()))
 #else
-      AS* g = static_cast<AS*>(group.get());
-#endif  // _UNSAFE_GROUP_CASTING
+      AS *g = static_cast<AS *>(group.get());
+#endif // _UNSAFE_GROUP_CASTING
       {
         node["groups"].push_back(*g);
       }
     }
     return node;
   }
-  static bool decode(const Node& node, Scenario& rhs) {
+  static bool decode(const Node &node, Scenario &rhs) {
     decode_properties(node, rhs);
     if (node["groups"]) {
       if (node["groups"].IsSequence()) {
-        for (const auto& c : node["groups"]) {
+        for (const auto &c : node["groups"]) {
           auto group = std::make_unique<AS>();
           convert<AS>::decode(c, *group);
           rhs.groups.push_back(std::move(group));
@@ -56,12 +59,12 @@ struct convert_scenario {
       }
     }
     if (node["obstacles"]) {
-      for (const auto& c : node["obstacles"]) {
+      for (const auto &c : node["obstacles"]) {
         rhs.obstacles.push_back(c.as<Disc>());
       }
     }
     if (node["walls"]) {
-      for (const auto& c : node["walls"]) {
+      for (const auto &c : node["walls"]) {
         rhs.walls.push_back(c.as<Wall>());
       }
     }
@@ -69,13 +72,12 @@ struct convert_scenario {
   }
 };
 
-template <>
-void decode_properties(const Node& node, Scenario& obj) {
-  for (const auto& [name, property] : obj.get_properties()) {
+template <> void decode_properties(const Node &node, Scenario &obj) {
+  for (const auto &[name, property] : obj.get_properties()) {
     if (node[name]) {
       obj.property_samplers[name] = property_sampler(node[name], property);
     } else {
-      for (const auto& alt_name : property.deprecated_names) {
+      for (const auto &alt_name : property.deprecated_names) {
         if (node[alt_name]) {
           obj.property_samplers[name] =
               property_sampler(node[alt_name], property);
@@ -86,25 +88,23 @@ void decode_properties(const Node& node, Scenario& obj) {
   obj.decode(node);
 }
 
-template <>
-struct convert<Scenario> {
-  static Node encode(const Scenario& rhs) {
+template <> struct convert<Scenario> {
+  static Node encode(const Scenario &rhs) {
     return convert_scenario<>::encode(rhs);
   }
-  static bool decode(const Node& node, Scenario& rhs) {
+  static bool decode(const Node &node, Scenario &rhs) {
     return convert_scenario<>::decode(node, rhs);
   }
 };
 
-template <>
-struct convert<std::shared_ptr<Scenario>> {
-  static Node encode(const std::shared_ptr<Scenario>& rhs) {
+template <> struct convert<std::shared_ptr<Scenario>> {
+  static Node encode(const std::shared_ptr<Scenario> &rhs) {
     if (rhs) {
       return convert_scenario<>::encode(*rhs);
     }
     return Node();
   }
-  static bool decode(const Node& node, std::shared_ptr<Scenario>& rhs) {
+  static bool decode(const Node &node, std::shared_ptr<Scenario> &rhs) {
     rhs = make_type_from_yaml<Scenario>(node);
     if (!rhs) {
       rhs = std::make_shared<Scenario>();
@@ -114,6 +114,6 @@ struct convert<std::shared_ptr<Scenario>> {
   }
 };
 
-}  // namespace YAML
+} // namespace YAML
 
-#endif  // NAVGROUND_SIM_YAML_SCENARIO_H
+#endif // NAVGROUND_SIM_YAML_SCENARIO_H

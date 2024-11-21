fece3eac850b63f4c26e188b88983a68f8776880
Jérôme Guzzi <jerome@idsia.ch>
2024-11-21 22:20:35 +0100

    Added __dict__ to pickle protocol
    Now the porting of the Python base classes from Python to C++ is complete.

--- navground_core_py/include/navground/core_py/pickle.h
+++ navground_core_py/include/navground/core_py/pickle.h
@@ -6,24 +6,34 @@
 #include "yaml.h"
 
 namespace py = pybind11;
-// 
-template <typename S, typename C>
-void pickle_via_yaml(C &cls) {
+//
+template <typename S, typename C> void pickle_via_yaml(C &cls) {
   cls.def(py::pickle(
       [](typename C::type *obj) {
         // __getstate__
+        auto py_obj = py::cast(obj);
+        if (py::hasattr(py_obj, "__dict__")) {
+          return py::make_tuple(YAML::dump<typename S::Native>(obj),
+                                py_obj.attr("__dict__"));
+        }
         return py::make_tuple(YAML::dump<typename S::Native>(obj));
       },
-      [](py::tuple t) {
+      [](const py::tuple &t) {
         // __setstate__
-        if (t.size() != 1) throw std::runtime_error("Invalid state!");
-        const YAML::Node node = YAML::Load(t[0].cast<std::string>());
-        auto obj = YAML::load_node_py<S>(node);
-        return obj.template cast<std::shared_ptr<typename C::type>>();
+        if (t.size() > 0) {
+          const YAML::Node node = YAML::Load(t[0].cast<std::string>());
+          auto obj = YAML::load_node_py<S>(node);
+          auto cpp_obj = obj.template cast<std::shared_ptr<typename C::type>>();
+          py::dict py_state;
+          if (t.size() > 1) {
+            py_state = t[1].cast<py::dict>();
+          }
+          return std::make_pair(cpp_obj, py_state);
+        }
+        throw std::runtime_error("Invalid state!");
       }));
 }
-template <typename S, typename C>
-void pickle_via_yaml_native(C &cls) {
+template <typename S, typename C> void pickle_via_yaml_native(C &cls) {
   cls.def(py::pickle(
       [](typename C::type *obj) {
         // __getstate__
@@ -31,10 +41,11 @@ void pickle_via_yaml_native(C &cls) {
       },
       [](py::tuple t) {
         // __setstate__
-        if (t.size() != 1) throw std::runtime_error("Invalid state!");
+        if (t.size() != 1)
+          throw std::runtime_error("Invalid state!");
         const YAML::Node node = YAML::Load(t[0].cast<std::string>());
         return node.as<S>();
       }));
 }
 
-#endif  // NAVGROUND_CORE_PY_PICKLE_H
+#endif // NAVGROUND_CORE_PY_PICKLE_H

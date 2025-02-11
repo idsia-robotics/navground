70aa6f472102a7899878a91140aacb1f04773f22
Jérôme Guzzi <jerome@idsia.ch>
2025-02-11 15:17:03 +0100

    Completed release images

--- docker/entrypoint_dev.sh
+++ docker/entrypoint_dev.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+
+source "/opt/ros/rolling/setup.bash" --
 source "/opt/navground/setup.bash" --
 source "/navground_venv/bin/activate" --
+source "/ws/install/setup.bash" --
+export NAVGROUND_PLUGINS_PREFIX=/ws/install:$NAVGROUND_PLUGINS_PREFIX
+export CMAKE_PREFIX_PATH=/opt/ros/rolling:$CMAKE_PREFIX_PATH
 exec "$@"
--- docker/entrypoint_release.sh
+++ docker/entrypoint_release.sh
@@ -3,5 +3,4 @@ set -e
 
 source "/opt/navground/setup.bash" --
 source "/navground_venv/bin/activate" --
-source "/ws/install/setup.bash" --
 exec "$@"

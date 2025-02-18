95e0112bd17fa3fe46625e2760c4a2b774503e81
Jérôme Guzzi <jerome@idsia.ch>
2025-02-18 17:17:19 +0100

    Add build and smoke tests on pull-request (#2)
    Improved individual packages CMakeLists.txt:
    
    - Added CTest
    - Added CPack
    - Added options to python_install_namespace_package to overwrite setup.cfg
    - Added wheel build and installation to python_install_namespace_package
    - Added source dependencies to generate_docstrings to avoid rebuilding docstrings.h.

--- cmake/generate_docstrings.cmake
+++ cmake/generate_docstrings.cmake
@@ -3,16 +3,24 @@ function(generate_docstrings target destination python_src headers)
   # CMAKE_CURRENT_FUNCTION_LIST_DIR is only available in cmake>=3.17
   set(script "${CMAKE_CURRENT_LIST_DIR}/../cmake/generate_docstrings.py")
 
+  file(GLOB_RECURSE file_headers ${headers}/**.h)
+
   # message("Python3_EXECUTABLE ${Python3_EXECUTABLE}")
-  add_custom_target(
-      ${target}_docstrings ALL
+  add_custom_command(
       COMMAND 
         ${Python3_EXECUTABLE} 
         # ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/generate_docstrings.py 
         ${script}
         ${destination} ${python_src} ${headers} ${ARGN} 
-      COMMENT "Generate docstrings" 
       OUTPUT ${destination}
+      DEPENDS ${python_src} ${file_headers}
   )
+
+  add_custom_target(
+      ${target}_docstrings
+      DEPENDS ${destination}
+      COMMENT "Generate docstrings" 
+  ) 
+
   add_dependencies(${target} ${target}_docstrings)
 endfunction()
---
+++ navground_examples_py/tests/test_discovery.py
@@ -0,0 +1,23 @@
+def test_behavior():
+    import navground_examples_py.behavior
+    navground_examples_py.behavior.main()
+
+
+def test_controller():
+    import navground_examples_py.controller
+    navground_examples_py.controller.main()
+
+
+def test_run():
+    import navground_examples_py.run
+    navground_examples_py.run.main()
+
+
+def test_experiment():
+    import navground_examples_py.experiment
+    navground_examples_py.experiment.main()
+
+
+def test_custom_yaml():
+    import navground_examples_py.custom_yaml
+    navground_examples_py.custom_yaml.main()

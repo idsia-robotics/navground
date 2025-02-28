05e5016461427dfa6801b0764820babea284bfda
Jérôme Guzzi <jerome@idsia.ch>
2025-02-28 10:38:25 +0100

    Version 0.5.1

--- navground_examples_py/setup.py
+++ navground_examples_py/setup.py
@@ -4,7 +4,7 @@ package_name = 'navground_examples_py'
 
 setup(
     name=package_name,
-    version='0.5.0',
+    version='0.5.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
